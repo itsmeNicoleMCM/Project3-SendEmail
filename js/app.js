b527736afd4bd3e00fa3c1420ac7d8e51ca2d414
itsmenicolemae <itsmenicolem.c.m.015@gmail.com>
2021-04-30 20:52:12 +0800

    Validating Fields Part 1

--- js/app.js
+++ js/app.js
@@ -1,23 +1,40 @@
 // Variables
 const sendBtn = document.getElementById('sendBtn');
+email = document.getElementById('email'),
+subject = document.getElementById('subject'),
+message = document.getElementById('message');
 
-	
 
 // Event Listeners
 eventListeners();
 
 
 function eventListeners(){
-  // App init
-  document.addEventListener('DOMContentLoaded', appInit);
+// App init
+document.addEventListener('DOMContentLoaded', appInit);
 }
+
+// Validate the forms
+email.addEventListener('blur', validateField);
+subject.addEventListener('blur', validateField);
+message.addEventListener('blur', validateField);
+
 
 
 // Functions
 
 // App intitialization
 function appInit() {
-  // disable the send button on load
-  sendBtn.disabled = true;
+// disable the send button on load
+sendBtn.disabled = true;
+}
+
+// Validate the form fields
+function validateField() {
+let errors;
+
+
+// Validate the length of the field
+validateLength(this);
 }
 
