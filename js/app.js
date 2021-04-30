a4a9d53f1ed142ed4ae341a2f83c01029948b5b5
itsmenicolemae <itsmenicolem.c.m.015@gmail.com>
2021-04-30 21:09:00 +0800

    Displaying second gif when email is sent and restart form

--- js/app.js
+++ js/app.js
@@ -44,14 +44,34 @@ e.preventDefault();
 // show the spinner
 const spinner = document.querySelector('#spinner');
 spinner.style.display = 'block';
+
+
+// show the gif image
+const sendEmailImg = document.createElement('img');
+sendEmailImg.src = 'img/mail.gif';
+sendEmailImg.style.display = 'block';
+
+
+// hide spinner then show the send email image
+setTimeout(function() {
+// hide the spinner
+spinner.style.display = 'none';
+
+
+// show the gif image
+document.querySelector('#loaders').appendChild( sendEmailImg );
+
+
+// After 5 seconds, hide the image and reset the form
+setTimeout(function() {
+// hide the spinner
+sendEmailForm.reset();    
+sendEmailImg.remove();  
+}, 5000 );
+
+
+}, 3000 );
 }
-
-    // hide spinner then show the send email image
-    setTimeout(function() {
-        // hide the spinner
-        spinner.style.display = 'none';
-    }, 3000 );
-
 
 // Validate the form fields
 function validateField() {
@@ -72,37 +92,36 @@ errors = document.querySelectorAll('.error');
 
 // Check that the inputs are not empty
 if(email.value !== '' && subject.value !== '' && message.value !== '' ) {
-    if(errors.length === 0) {
-        // the button should be enabled
-        sendBtn.disabled = false;
-        }
-    }
+if(errors.length === 0) {
+// the button should be enabled
+sendBtn.disabled = false;
+}
+}
 }
 // Validate the length of the fields
 function validateLength(field) {
-    if(field.value.length > 0 ) {
-        field.style.borderBottomColor = 'green';
-        field.classList.remove('error');
-    } else {
-    field.style.borderBottomColor = 'red';
-    field.classList.add('error');
-    }
+if(field.value.length > 0 ) {
+field.style.borderBottomColor = 'green';
+field.classList.remove('error');
+} else {
+field.style.borderBottomColor = 'red';
+field.classList.add('error');
+}
 }
 // Validate email (checks for @ in the value)
-    function validateEmail(field) {
-    let emailText = field.value;
+function validateEmail(field) {
+let emailText = field.value;
 // check if the emailText contains the @ sign
-    if(emailText.indexOf('@') !== -1) {
-        field.style.borderBottomColor = 'green';
-        field.classList.remove('error');
-    } else {
-    field.style.borderBottomColor = 'red';
-    field.classList.add('error');
-    }
+if(emailText.indexOf('@') !== -1) {
+field.style.borderBottomColor = 'green';
+field.classList.remove('error');
+} else {
+field.style.borderBottomColor = 'red';
+field.classList.add('error');
+}
 }
 
 // Reset The Form
 function resetForm() {
 sendEmailForm.reset();
 }
-
