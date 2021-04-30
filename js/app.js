fca2c43bfce79e8f003b19f68af52c2fed418b2d
itsmenicolemae <itsmenicolem.c.m.015@gmail.com>
2021-04-30 20:56:33 +0800

    Validating Fields part 2

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const sendBtn = document.getElementById('sendBtn');
 email = document.getElementById('email'),
 subject = document.getElementById('subject'),
 message = document.getElementById('message');
+reset = document.getElementById('resetbtn'),	
+
 
 
 // Event Listeners
@@ -36,5 +38,32 @@ let errors;
 
 // Validate the length of the field
 validateLength(this);
+
+// Validate email
+if(this.type === 'email') {
+validateEmail(this);
+}
+}
+// Validate the length of the fields
+function validateLength(field) {
+if(field.value.length > 0 ) {
+field.style.borderBottomColor = 'green';
+field.classList.remove('error');
+} else {
+field.style.borderBottomColor = 'red';
+field.classList.add('error');
+}
+}
+// Validate email (checks for @ in the value)
+function validateEmail(field) {
+let emailText = field.value;
+// check if the emailText contains the @ sign
+if(emailText.indexOf('@') !== -1) {
+field.style.borderBottomColor = 'green';
+field.classList.remove('error');
+} else {
+field.style.borderBottomColor = 'red';
+field.classList.add('error');
+}
 }
 
