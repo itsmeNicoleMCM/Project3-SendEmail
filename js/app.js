84d646d76939ee8b70f8c7b66d69928b345d21b2
itsmenicolemae <itsmenicolem.c.m.015@gmail.com>
2021-04-30 20:57:54 +0800

    Validating Part 2: Changing ';' to ','

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // Variables
-const sendBtn = document.getElementById('sendBtn');
+const sendBtn = document.getElementById('sendBtn'),
 email = document.getElementById('email'),
 subject = document.getElementById('subject'),
-message = document.getElementById('message');
-reset = document.getElementById('resetbtn'),	
+message = document.getElementById('message'),
+reset = document.getElementById('resetbtn');	
 
 
 
@@ -27,43 +27,42 @@ message.addEventListener('blur', validateField);
 
 // App intitialization
 function appInit() {
-// disable the send button on load
+// disable the send button on loads
 sendBtn.disabled = true;
 }
 
 // Validate the form fields
 function validateField() {
-let errors;
+    let errors;
 
+    // Validate the length of the field
+    validateLength(this);
 
-// Validate the length of the field
-validateLength(this);
-
-// Validate email
-if(this.type === 'email') {
-validateEmail(this);
-}
+    // Validate email
+    if(this.type === 'email') {
+        validateEmail(this);    
+    }
 }
 // Validate the length of the fields
 function validateLength(field) {
-if(field.value.length > 0 ) {
-field.style.borderBottomColor = 'green';
-field.classList.remove('error');
-} else {
-field.style.borderBottomColor = 'red';
-field.classList.add('error');
-}
+    if(field.value.length > 0 ) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
 }
 // Validate email (checks for @ in the value)
 function validateEmail(field) {
-let emailText = field.value;
-// check if the emailText contains the @ sign
-if(emailText.indexOf('@') !== -1) {
-field.style.borderBottomColor = 'green';
-field.classList.remove('error');
-} else {
-field.style.borderBottomColor = 'red';
-field.classList.add('error');
-}
+    let emailText = field.value;
+    // check if the emailText contains the @ sign
+        if(emailText.indexOf('@') !== -1) {
+            field.style.borderBottomColor = 'green';
+            field.classList.remove('error');
+        } else {
+            field.style.borderBottomColor = 'red';
+            field.classList.add('error');
+        }
 }
 
